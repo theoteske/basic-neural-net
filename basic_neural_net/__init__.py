21c05fae7411db65d7b1a64c4d281f46898a13f4
Theo Teske <theo.teske@gmail.com>
2025-02-18 04:33:59 -0600

    added setup.py and __init__.py to make package installable

---
+++ basic_neural_net/__init__.py
@@ -0,0 +1,9 @@
+"""A NumPy-based neural network implementation built from scratch."""
+
+from . import activations
+from . import layers
+from . import losses
+from . import optimizers
+from .nn import Sequential
+
+__version__ = "0.1.0"
